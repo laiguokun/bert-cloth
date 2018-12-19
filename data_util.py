330ebe3afdad26523ff01962ab0c68b4e1e8a045
Guokun Lai <glai@fpga2.csi.bnl.gov>
2018-12-19 18:01:48 -0500

    best hyper parameter

--- data_util.py
+++ data_util.py
@@ -206,7 +206,7 @@ if __name__ == '__main__':
     for item in data_collections:    
         args.data_dir = './CLOTH/{}'.format(item)
         args.pre = args.post = 0
-        args.bert_model = 'bert-base-uncased'
+        args.bert_model = 'bert-large-uncased'
         args.save_name = './data/{}-{}.pt'.format(item, args.bert_model)
         data = Preprocessor(args)
     '''
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=2 python -u main.py --output_dir debug-exp/ --do_eval 
+CUDA_VISIBLE_DEVICES=0 python -u main.py --output_dir debug-exp/ --do_eval --do_train --train_batch_size 4 --bert_model bert-large-uncased --learning_rate 1e-5 --num_train_epochs 2
